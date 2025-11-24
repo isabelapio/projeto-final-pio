22015034f5054d133611c296d5593f168f435e79
Melânia Gabrielli Silva Amaral <melaniagabriellis@gmail.com>
2025-11-24 09:00:38 -0300

    ncsadhjc

--- app/aboutme.js
+++ app/aboutme.js
@@ -1,20 +1,35 @@
-import { StyleSheet, Text, View, Image} from "react-native";
+import { StyleSheet, Text, View, Image, Platform } from "react-native";
 import foto from "../assets/pio.jpg";
 
 export default function Page() {
   return (
     <View style={styles.container}>
-      <View style={styles.main}>
+      
         <Text style={styles.title}>Sobre mim</Text>
-        <View>
-       <Image source={foto} style={styles.foto}/>
+        <View style = {styles.main}>
+          <View>
+            <Image source={foto} style={styles.foto} />
+          </View>
+          <View>
+            <Text style={styles.subtitle}>
+              <Text style = {{fontWeight: "bold"}}> Nome:</Text> Isabela Pio
+            </Text>
+            <Text style={styles.subtitle}>
+              <Text style = {{fontWeight: "bold"}}> RM:</Text> 08412
+            </Text>
+          <Text style={styles.subtitle}>
+              <Text style = {{fontWeight: "bold"}}> Endereço:</Text> Rua Aracajú 16-56
+            </Text>
+            <Text style={styles.subtitle}>
+              <Text style = {{fontWeight: "bold"}}> Cidade:</Text> Presidente Epitácio, SP
+            </Text>
+            <Text style={styles.subtitle}>
+              <Text style = {{fontWeight: "bold"}}> Estado:</Text> São Paulo
+            </Text>
+          </View>
         </View>
-        <View>
-      <Text style={styles.subtitle}>Segunda pagina.</Text>
-        </View>
-        
+
       </View>
-    </View>
   );
 }
 
@@ -23,13 +38,13 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     padding: 24,
-    backgroundColor: "#4a09ba"
+    backgroundColor: "#4a09ba",
   },
+
   main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
+    flexDirection: Platform.OS === "web" ? "row" : "column",
+    gap: 20,
+    alignItems: "center"
   },
   title: {
     fontSize: 64,
@@ -37,15 +52,13 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   subtitle: {
-    fontSize: 36,
-    color: "#38434D",
+    fontSize: 20,
+    color: "#111519",
   },
-  foto:{
-width: 200,
-height:200,
-borderRadius: 10,
-marginBottom: 20,
-
-
-  }
+  foto: {
+    width: 200,
+    height: 200,
+    borderRadius: 10,
+    //marginBottom: 20,
+  },
 });
